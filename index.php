e2784ca63823af4b9013ad6c61de520a7c3c23a9
nylser <korbinian.stein@gmail.com>
2012-08-13 10:17:06 +0200

    Finishing index.php for the dev. of the basic functions.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		<title>MineGuild - Deutscher Minecraft-Server</title>
 	</head>
 	<body>
-		<?php
-		?>
+		<p>Hier entsteht die neue Homepage von mineguild.de!</p>
+		<div align="center">Copyright (C) 2012 by MineGuild</div>
 	</body>
 </html>
